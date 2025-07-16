12d596cf0e7ea6c57a2e148e935872795dc76517
Mayank <maykat425@gmail.com>
2025-07-16 10:57:14 +0530

    commit from mayank

--- Arrays/Medium/Que2-Best Time to Buy and Sell Stock II.py
+++ Arrays/Medium/Que2-Best Time to Buy and Sell Stock II.py
@@ -14,7 +14,7 @@
 # Output: 7
 # Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 # Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
-# Total profit is 4 + 3 = 7.
+# Total profit is 4 + 3 = 7. 
 # Example 2:
 
 # Input: prices = [1,2,3,4,5]
